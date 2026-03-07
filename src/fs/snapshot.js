4276f471f07b72c4439a2a9f1cb66f7884b29f7a
natasyo <natasyo23@gmail.com>
2026-03-07 17:17:10 +0300

    feat: snapshot

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,38 @@
+import fs from "fs"
+import path from "path"
+
+
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+  let entries=[]
+const rootPath=process.cwd();
+
+  function readDirectory(pathDirectory){
+    const files=fs.readdirSync(pathDirectory);
+    for(let file of files){
+      const fullPath=path.join(pathDirectory, file);
+      const stat=fs.statSync(fullPath);
+      if(stat.isFile()){
+        const content=fs.readFileSync(fullPath,{encoding:"utf-8"});
+         entries.push({
+        path:path.relative(rootPath, fullPath),
+        type:'file',
+        size:stat.size,
+        content
+      })
+      }else{
+         entries.push({
+        path:path.relative(rootPath, fullPath),
+        type:"directory"})
+        readDirectory(fullPath);
+      }
+    }
+  }
+  readDirectory(rootPath)
+  const data={
+    rootPath,
+    entries
+  }
+  fs.writeFileSync("./src/fs/data.json",JSON.stringify(data, "",2))
 };
 
 await snapshot();
