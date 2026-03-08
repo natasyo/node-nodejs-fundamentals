8d12116ab5a132683a8ad486551bc76fbe493e94
Natallia <natasyo23@gmail.com>
2026-03-08 19:43:47 +0300

    feat: modules:dynamic

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -4,6 +4,10 @@ const dynamic = async () => {
   // Dynamically import plugin from plugins/ directory
   // Call run() function and print result
   // Handle missing plugin case
+
+  const command = process.argv[2];
+  const module = await import(`./plugins/${command}.js`);
+  console.log(module.run());
 };
 
 await dynamic();
