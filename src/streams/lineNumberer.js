9b77c5e88ae798076d686a8bc4cbcdbafb3511e5
Natallia <natasyo23@gmail.com>
2026-03-08 22:43:13 +0300

    feat: strams line

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,30 @@
+import { Transform } from "stream";
+
 const lineNumberer = () => {
   // Write your code here
   // Read from process.stdin
   // Use Transform Stream to prepend line numbers
   // Write to process.stdout
+  let buffer = "";
+  let lineNumber = 1;
+  const lineTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      buffer += chunk.toString();
+      const lines = buffer.split("\n");
+      buffer = lines.pop();
+      const result =
+        lines.map((line, index) => `${lineNumber++}: ${line}`).join("\n") +
+        "\n";
+      callback(null, result);
+    },
+    flush(callback) {
+      if (buffer.length > 0) {
+        this.push(`${lineNumber++}: ${buffer}\n`);
+      }
+      callback();
+    },
+  });
+  process.stdin.pipe(lineTransform).pipe(process.stdout);
 };
 
 lineNumberer();
