b8898a6cc72d606585676521ef54d32365a363b9
natasyo <natasyo23@gmail.com>
2026-03-10 20:23:50 +0300

    fix main

--- src/wt/main.js
+++ src/wt/main.js
@@ -29,13 +29,7 @@ function sortArrays(chunks) {
 }
 
 const main = async () => {
-  // Write your code here
-  // Read data.json containing array of numbers
-  // Split into N chunks (N = CPU cores)
-  // Create N workers, send one chunk to each
-  // Collect sorted chunks
-  // Merge using k-way merge algorithm
-  // Log final sorted array
+
 
   if (isMainThread) {
     const dataJson = await readFile(
